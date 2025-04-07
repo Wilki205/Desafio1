3fc9212d67658a0fe606386c80ade480f6d8793e
wilkison <wilkisonsouza167@gmail.com>
2025-04-07 06:20:56 -0300

    codigo simplificado para duas linhas, sem perder funcionalidade

---
+++ Desafio01simplificado.py
@@ -0,0 +1,2 @@
+eh01, em01, eh02, em02 = int(input("Hora: ")), int(input("Minuto: ")), int(input("Hora: ")), int(input("minuto: "))
+print(f"Saída às {(eh01 + eh02 + (em01 + em02) // 60) % 24:02d}:{(em01 + em02) % 60:02d}")
