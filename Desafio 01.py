d70bef5f39d9c8547edd9fbc788c348e78bef558
wilkison <wilkisonsouza167@gmail.com>
2025-04-07 18:16:51 -0300

    ultimos ajustes

--- Desafio 01.py
+++ Desafio 01.py
@@ -1,15 +1,20 @@
-eh01 = int(input("Digite a hora da entrada: "))
-em01 = int(input("Digite o minuto: "))
-eh02 = int(input("Digite a hora da entrada: "))
-em02 = int(input("Digite o minuto: "))
+eh01 = int(input("Hora: "))
+em01 = int(input("Minuto: "))
+eh02 = int(input("Hora: "))
+em02 = int(input("Minuto: "))
 
-somah = eh01+eh02
-somam = em01+em02
+somah = eh01 + eh02
+somam = em01 + em02
 
 if somam >= 60:
-        somah += somam // 60
-        somam = somam % 60
+    somah += somam // 60
+    somam = somam % 60
 
 somah = somah % 24
 
-print (f"Saida as {somah:02d}:{somam:02d}")
+if somah % 12 == 0:
+    hora12 = 12
+else:
+    hora12 = somah % 12
+
+print(f"Saída às {hora12:02d}:{somam:02d}")
--- Desafio01simplificado.py
+++ Desafio01simplificado.py
@@ -1,2 +1,2 @@
-eh01, em01, eh02, em02 = int(input("Hora: ")), int(input("Minuto: ")), int(input("Hora: ")), int(input("minuto: "))
-print(f"Saída às {(eh01 + eh02 + (em01 + em02) // 60) % 24:02d}:{(em01 + em02) % 60:02d}")
+eh01, em01, eh02, em02 = int(input("Hora: ")), int(input("Minuto: ")), int(input("Hora: ")), int(input("Minuto: "))
+print(f"Saída às {(eh01 + eh02 + (em01 + em02) // 60) % 12:02d}:{(em01 + em02) % 60:02d}")
